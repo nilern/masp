ce81e985ae280df72d49e3b1a0f7e41441864865
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-01-02 20:31:01 +0200

    Initial commit.

---
+++ clj/build.boot
@@ -0,0 +1,5 @@
+(set-env!
+  :source-paths #{"src"}
+  :resource-paths #{"resources"}
+  :dependencies '[[org.clojure/clojure "1.7.0"]
+                  [instaparse "1.4.5"]])
---
+++ clj/src/masp/core.clj
@@ -0,0 +1,104 @@
+(ns masp.core
+  (:refer-clojure :exclude [read-string eval])
+  (:require [instaparse.core :as insta]))
+
+;;;; Read
+
+(def parser
+  (insta/parser (clojure.java.io/resource "masp.ebnf")))
+
+(def ptree->sexpr
+  (partial insta/transform
+    {:expr identity
+     :list #(apply list %&)
+     :number #(Integer/parseInt %)
+     :symbol symbol}))
+
+(def read-string
+  (comp ptree->sexpr parser))
+
+;;;; Eval
+
+(defprotocol Operative
+  (operate [op operand dyn-env cont]))
+
+(deftype CompoundOp [name formal eformal body lex-env]
+  Operative
+  (operate [op operand dyn-env cont]
+    (let [env (-> lex-env
+                  (assoc name op)
+                  (assoc formal operand)
+                  (assoc eformal dyn-env))]
+      [:eval body env cont])))
+
+(deftype PrimOp [f]
+  Operative
+  (operate [_ operand dyn-env cont]
+    (f operand dyn-env cont)))
+
+(deftype Applicative [op]
+  Operative
+  (operate [_ operand dyn-env cont]
+    (if (seq operand)
+      [:eval (first operand) dyn-env
+       (cons [:arg dyn-env op '() (rest operand)] cont)]
+      [:apply op operand dyn-env cont])))
+
+(defmacro mfn [formals body]
+  (let [cont (gensym 'cont), ignore (gensym)]
+    `(Applicative. (PrimOp. (fn [~formals ~ignore ~cont]
+                              [:continue ~body ~cont])))))
+
+(defn eval
+  ([ctrl env]
+    (eval ctrl env (list [:halt])))
+  ([ctrl env cont]
+    (loop [state [:eval ctrl env cont]]
+      (case (first state)
+        :eval
+        (let [[_ ctrl env cont] state]
+          (cond
+            (and (seq? ctrl) (seq ctrl))
+            (recur [:eval (first ctrl) env (cons [:op env (rest ctrl)] cont)])
+
+            (symbol? ctrl)
+            (if-let [value (get env ctrl)]
+              (recur [:continue value cont])
+              [:err [:unbound ctrl]])
+
+            :else
+            (recur [:continue ctrl cont])))
+
+        :continue
+        (let [[_ value cont] state]
+          (case (ffirst cont)
+            :arg
+            (let [[_ env op args operands] (first cont)]
+              (if (seq operands)
+                (recur
+                  [:eval (first operands) env
+                   (cons [:arg env op (cons value args) (rest operands)]
+                         (rest cont))])
+                (recur
+                  [:apply op (reverse (cons value args)) env (rest cont)])))
+
+            :op
+            (let [[_ env operand] (first cont)]
+              (recur [:apply value operand env (rest cont)]))
+
+            :halt
+            [:ok value]))
+
+        :apply
+        (let [[_ op operand env cont] state]
+          (recur (operate op operand env cont)))))))
+
+(defn op [[name formal eformal body] env cont]
+  [:continue (CompoundOp. name formal eformal body env) cont])
+
+(def default-env
+  {(symbol "##sf#op") (PrimOp. op)
+
+   (symbol "##intr#head") (mfn [[x]] x)
+   (symbol "##intr#tail") (mfn [[_ & xs]] xs)
+   (symbol "##intr#cons") (mfn [x xs] (cons x xs))})
