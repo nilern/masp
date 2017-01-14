22b196562651a41205dbaafe4687a5f9f4e34b13
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-01-14 23:22:06 +0200

    Minor cleanup.

--- clj/src/masp/core.clj
+++ clj/src/masp/core.clj
@@ -9,21 +9,13 @@
            [masp.env SDAEnv]))
 
 ;;; FIXME: eval(*) should be [Env Expr] -> Any instead of [Expr Env] -> Any
-
-(defn- op [[name formal eformal & body] env cont]
-  [:continue (CompoundOp. name formal eformal body env) nil nil cont])
-
-(defn- begin [[stmt & stmts] env cont]
-  (if stmt
-    (if stmts
-      (let [cont* (into cont [env stmts :stmt])]
-        [:eval stmt nil env cont*])
-      [:eval stmt nil env cont])
-    [:continue () nil nil cont]))
+;;; TODO: use CPS to handle #@ and #% errors, i.e. (#%iadd a b (@fn err ...))
 
 (def default-bindings
-  {(symbol "#@op") (PrimOp. op)
-   ;(symbol "#@begin") (PrimOp. begin)
+  {(symbol "#@op") (PrimOp.
+                     (fn [[name formal eformal & body] env cont]
+                       [:continue (CompoundOp. name formal eformal body env)
+                                  nil nil cont]))
 
    (symbol "#%wrap") (mfn [f] (Applicative. f))
    (symbol "#%unwrap") (mfn [f] (.op f))
@@ -34,14 +26,14 @@
                           (let [k (Continuation. cont)]
                             [:combine f [k] env cont]))
    (symbol "#%throw") (mfn* [[k v] _ _] [:continue v nil nil (.cont k)])
-   ;; TODO: fail if (not= (peek cont) :stmt)
-   ;; OR?: always have an env at (get cont (- (count cont) 2))
    (symbol "#%cont/env") (mfn* [[value env*] _ cont]
-                           (let [env (get cont (- (count cont) 3))]
-                             (if (env/scope= env env*)
+                           (let [cont-label (peek cont)
+                                 env (get cont (- (count cont) 3))]
+                             (if (and (= cont-label :stmt)
+                                      (env/scope= env env*))
                                (let [cont* (assoc cont (- (count cont) 3) env*)]
                                  [:continue value nil nil cont*])
-                               [:err [:scope env env*]])))
+                               [:err [:scope cont-label env env*]])))
    (symbol "#%err") (mfn* [[value] _ cont]
                       [:continue value nil nil [:err]])
 
